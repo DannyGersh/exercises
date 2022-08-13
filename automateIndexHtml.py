574307f4d9c52fa7f27fcee44d1ba3a6239ec3d3
danny <dannygershman96@gmail.com>
2022-08-13 18:01:14 +0300

    automate index.html production step

---
+++ automateIndexHtml.py
@@ -0,0 +1,26 @@
+# this script transforms a dev/page to a production version
+# usege:
+# copy this file inside a page in dev
+# for example into /dev/new
+# and execute with no args
+
+import re
+from os import rename, system, getcwd, path
+from shutil import copytree
+
+page = path.basename(getcwd())
+	
+if path.exists(page):	
+	system('rmdir /s /q %s'%page)
+
+copytree('build', page)
+	
+rename('%s/index.html'%page, '%s/%s.html'%(page,page))
+
+data = ''
+with open('%s/%s.html'%(page,page), 'r') as f:
+	data = f.read()
+with open('%s/%s.html'%(page,page), 'w', newline='') as f:
+	res = data.replace('/static/js/','/static/pages/%s/static/js/'%page)
+	data = f.write(res)
+		
