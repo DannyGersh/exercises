43c7016905bf5440b755089b6555f6f1c5da1dec
DannyGersh <51251017+DannyGersh@users.noreply.github.com>
2022-09-17 20:10:34 +0300

    Update createChalanges.sql

--- production/volume/temp/createChalanges.sql
+++ production/volume/temp/createChalanges.sql
@@ -1,16 +1,16 @@
 \c exercises
 
 create table	chalanges(
-id         		serial			primary key not null     	,
+id         		serial			    primary key not null     	,
 exercise  		varchar(4000) 	not null                 	,
 answer    		varchar(4000) 	not null                 	,
 hints       	varchar(4000)                           	,
-author      	integer		                            	,
-creationdate	timestamp 		default current_timestamp	,
-title       	varchar(400) 	not null                 	,
-rating      	integer[] 		not null default '{}'		,
-tags        	integer[]		not null default '{}'		,
-explain     	varchar(4000)								,
-latex			varchar(30)									,
-latexp			varchar(400)
+author      	integer		                            	  ,
+creationdate	timestamp 		default current_timestamp	  ,
+title       	varchar(400) 	not null                  	,
+rating      	integer[] 		not null default '{}'		    ,
+tags        	integer[]		  not null default '{}'		    ,
+explain     	varchar(4000)								              ,
+latex			    varchar(30)									              ,
+latexp			  varchar(400)
 );
