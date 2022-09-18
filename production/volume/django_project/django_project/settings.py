033c65c166497b82232cf0be7630584d1c64ee2d
root <dannygershman96@gmail.com>
2022-09-18 17:06:10 +0300

    created django_secret_key.py

--- production/volume/django_project/django_project/settings.py
+++ production/volume/django_project/django_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import DJANGO_SECRET_KEY from . env_django
+from . django_secret_key import DJANGO_SECRET_KEY
 
 from pathlib import Path
 
